f1e67ac62792112631493736665f5c7f3c8f9170
Ivanq0 <119148437+Ivanq0@users.noreply.github.com>
2026-04-17 13:37:56 +0600

    Delete add directory

--- add/add.c
+++
@@ -1,8 +0,0 @@
-#include "add.h"
-#include <stdio.h>
-
-int add_int(int a, int b) { return a + b; }
-
-double add_double(double a, double b) { return a + b; }
-
-int minus_int(int a, int b) { return a - b; }
